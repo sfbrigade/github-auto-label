87e78ecc753788f1bf65d806a4ebe2829b27a25f
Trent Oswald <trentoswald@therebelrobot.com>
2016-04-01 16:00:05 -0700

    feat(api): added api call for labels, docs

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # github-auto-label
 A small Node.js webhook service to auto-label new issues or PRs on submission
+
+This micro-service depends on enviroment variables being set. Listed below are the required values.
+
+Environment Variable | Description | Default
+-------------------- | ----------- | -------
+`GITHUB_REPOS` | A comma-delimited list of repos to watch for (e.g. `github-auto-label` or `github-auto-label,github-add`) | undefined
+`GITHUB_PR_LABELS` | A comma-delimited list of labels to attach to new pull requests (leave empty to disable PR labeling) | undefined
+`GITHUB_ISSUE_LABELS` | A comma-delimited list of labels to attach to new issues (leave empty to disable issue labeling) | undefined
+`GITHUB_TOKEN` | A valid Github Access Token (Create a personal one here: https://github.com/settings/tokens/new) | undefined
+`WEBHOOK_PATH` | Server path for the webhook | `/`
+`GITHUB_SECRET` | Passphrase set up during Webhook addition | `thisIsASuperSecretSecret`
+`PORT` | Server port | `80`
--- github-auto-label.js
+++ github-auto-label.js
@@ -6,46 +6,86 @@ var bodyParser = require('body-parser')
 var chalk = require('chalk')
 var debug = require('debug-log')('github-auto-label')
 
-var repos = process.env.GITHUB_REPOS || 'github-auto-label'
+var repos = process.env.GITHUB_REPOS
 if (repos.indexOf(',') > -1) {
   repos = repos.split(',')
 } else {
   repos = [repos]
 }
 
-var label = {
-  pr: process.env.GITHUB_PR_LABEL || 'needs-review',
-  issue: process.env.GITHUB_ISSUE_LABEL || 'needs-triage'
+var labels = {
+  pr: process.env.GITHUB_PR_LABELS,
+  issue: process.env.GITHUB_ISSUE_LABELS
 }
+
+if (!labels.pr || labels.pr === '') {
+  labels.pr = []
+} else if (labels.pr.indexOf(',') > -1) {
+  labels.pr = labels.pr.split(',')
+} else {
+  labels.pr = [labels.pr]
+}
+if (!labels.issue || labels.issue === '') {
+  labels.issue = []
+} else if (labels.issue.indexOf(',') > -1) {
+  labels.issue = labels.issue.split(',')
+} else {
+  labels.issue = [labels.issue]
+}
+
+var accessToken = process.env.GITHUB_TOKEN
+// Personal Access Token: https://github.com/settings/tokens/new
 
 var webhookSettings = {
   path: process.env.WEBHOOK_PATH || '/',
   secret: process.env.GITHUB_SECRET || 'thisIsASuperSecretSecret'
-// Personal Access Token: https://github.com/settings/tokens/new
 }
 
 var webhookHandler = GithubWebHook(webhookSettings)
 
 var app = express()
 app.set('port', process.env.PORT || 5555)
-app.use(bodyParser.json()) // must use bodyParser in express
-app.use(webhookHandler) // use our middleware
+app.use(bodyParser.json())
+app.use(webhookHandler)
 
 webhookHandler.on('pull_request', function (repo, data) {
-  // Event name https://developer.github.com/webhooks/#events
-  console.log(repo, data)
-  if (repos.indexOf(repo) < 0 || data.action !== 'opened') return
-  console.log('[%s] Incoming webhook. adding label [%s] to %s#%s', chalk.yellow('github-auto-label'), label.pr, repo, data.pull_request.number)
+  if (repos.indexOf(repo) < 0 || data.action !== 'opened' || labels.pr.length === 0) return
+  debug('[%s] Incoming webhook. adding labels %s to %s#%s', chalk.yellow('github-auto-label'), JSON.stringify(labels.pr), repo, data.pull_request.number)
+  var opts = {
+    method:'POST',
+    uri: data.pull_request.issue_url + '/labels',
+    headers: {
+      'User-Agent': 'Github-Auto-Label',
+      'Authorization': 'token '+accessToken,
+      'Content-Type': 'application/json'
+    },
+    form: JSON.stringify(labels.pr)
+  }
+  request(opts, function(err, results, body){
+    if (err) console.error(err)
+    debug('[%s] API response %s', chalk.magenta('github'), JSON.stringify(body, null, ' '))
+  })
 })
 webhookHandler.on('issues', function (repo, data) {
-  // Event name https://developer.github.com/webhooks/#events
-  console.log(repo, data)
-  if (repos.indexOf(repo) < 0 || data.action !== 'opened') return
-  console.log('[%s] Incoming webhook. adding label [%s] to %s#%s', chalk.yellow('github-auto-label'), label.issue, repo, data.issue.number)
+  if (repos.indexOf(repo) < 0 || data.action !== 'opened' || labels.issue.length === 0)) return
+  debug('[%s] Incoming webhook. adding labels %s to %s#%s', chalk.yellow('github-auto-label'), JSON.stringify(labels.issue), repo, data.issue.number)
+  var opts = {
+    method:'POST',
+    uri: data.issue.url + '/labels',
+    headers: {
+      'User-Agent': 'Github-Auto-Label',
+      'Authorization': 'token '+accessToken,
+      'Content-Type': 'application/json'
+    },
+    form: JSON.stringify(labels.issue)
+  }
+  request(opts, function(err, results, body){
+    if (err) console.error(err)
+    debug('[%s] API response %s', chalk.magenta('github'), JSON.stringify(body, null, ' '))
+  })
 })
 
 webhookHandler.on('error', function (err, req, res) {
-  // something went wrong
   console.error('an error occurred', err)
 })
 
